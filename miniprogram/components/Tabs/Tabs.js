d1d8e7dfb5e669a56c53fe61c8a96086e3bd3b8c
shuoxv <xv_shuo@outlook.com>
2021-12-21 21:24:56 +0800

    生命周期

--- miniprogram/components/Tabs/Tabs.js
+++ miniprogram/components/Tabs/Tabs.js
@@ -4,35 +4,16 @@ Component({
      * 组件的属性列表
      */
     properties: {
-
+        tabs:{
+            type:Array,
+            value:[]
+        }
     },
 
     /**
      * 组件的初始数据
      */
     data: {
-        tabs:[
-            {
-                id:0,
-                name:"首页",
-                isActive:true
-            },
-            {
-                id:1,
-                name:"原创",
-                isActive:false
-            },
-            {
-                id:2,
-                name:"分类",
-                isActive:false
-            },
-            {
-                id:3,
-                name:"关于",
-                isActive:false
-            }
-        ]
     },
 
     /**
@@ -46,15 +27,18 @@ Component({
             // 4 对数组循环
             //     1 给每一个循环属性 选中属性改为false
             //     2 给当前的索引项设置为true
+            //     3 点击事件触发的时候，触发父组件中的自定义事件，同时传递数据给父组件
+            // this.triggerEvent("父组件自定义事件的名称"，要传递的参数)
             const {index} = e.currentTarget.dataset
-            console.log(index)
-            let {tabs} = this.data
-            tabs.forEach((v,i)=>{
-                i===index?v.isActive=true:v.isActive=false
-            })
-            this.setData({
-                tabs
-            })
+            this.triggerEvent("itemChange",{index})
+            // console.log(index)
+            // let {tabs} = this.data
+            // tabs.forEach((v,i)=>{
+            //     i===index?v.isActive=true:v.isActive=false
+            // })
+            // this.setData({
+            //     tabs
+            // })
         },
     }
 })
